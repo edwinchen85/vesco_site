49c25778d72203a0cc13d0f08da98a35f0d5408b
Edwin Chen <edwinchen85@gmail.com>
2017-06-27 16:51:43 +0800

    Activate magnificPopup and define settings

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,11 @@ $(function() {
   Work
 =============================================*/
 $(function() {
-
+  $("#work").magnificPopup({
+    delegate: 'a',  // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
 });
