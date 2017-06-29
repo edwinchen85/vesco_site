8896b024bc40a87eb5ccad94f0cc316376f73d0f
Edwin Chen <edwinchen85@gmail.com>
2017-06-29 22:48:12 +0800

    Close mobile menu on click

--- js/custom.js
+++ js/custom.js
@@ -94,3 +94,10 @@ $(function() {
     }, 1250, "easeInOutExpo");
   });
 });
+
+// Close mobile menu on click
+$(function() {
+  $(".navbar-collapse ul li a").on("click touch", function() {
+    $(".navbar-toggle").click();
+  });
+});
